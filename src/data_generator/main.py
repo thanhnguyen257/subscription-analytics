7cf71edf87fc9834c6a755c2b9f23061666c60ff
Nguyễn Lê Tuấn Thành <thanhnlt4@fpt.com>
2026-04-13 14:26:54 +0700

    generate subscription plans data

--- src/data_generator/main.py
+++ src/data_generator/main.py
@@ -1,5 +1,7 @@
 from users import generate_users
 from products import generate_products
+from plans import generate_plans
+
 import os
 
 OUTPUT_DIR = "data/master_db"
@@ -9,13 +11,13 @@ def main():
 
     users_df = generate_users()
     products_df = generate_products()
+    plans_df = generate_plans(products_df)
 
     users_df.to_csv(f"{OUTPUT_DIR}/users.csv", index=False)
     products_df.to_csv(f"{OUTPUT_DIR}/products.csv", index=False)
+    plans_df.to_csv(f"{OUTPUT_DIR}/plans.csv", index=False)
 
     print("Data generated successfully!")
-    print(f"- Users: {len(users_df)}")
-    print(f"- Products: {len(products_df)}")
 
 if __name__ == "__main__":
     main()
