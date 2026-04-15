80c085aff9aac44a910926cd808103ad4f96bdf3
Tuan Thanh <nltuanthanh@gmail.com>
2026-04-15 14:12:01 +0700

    add landing schema to postgres

--- docker/postgres/init_db.sh
+++ docker/postgres/init_db.sh
@@ -12,9 +12,14 @@ psql "$CONN_STR" -c "CREATE DATABASE ${POSTGRES_DB}"
 
 POSTGRES_CONN_STR="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
+psql "$POSTGRES_CONN_STR" \
+-c "CREATE SCHEMA ${POSTGRES_SCHEMA_SOURCE};"
+psql "$POSTGRES_CONN_STR" \
+-c "CREATE SCHEMA ${POSTGRES_SCHEMA_LANDING};"
+
 psql "$POSTGRES_CONN_STR" <<-EOSQL
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.users (
     user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
@@ -27,14 +32,14 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP WITHOUT TIME ZONE
 );
 
-CREATE TABLE IF NOT EXISTS products (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.products (
     product_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     product_name VARCHAR(100),
     category VARCHAR(50),
     created_at TIMESTAMP WITHOUT TIME ZONE
 );
 
-CREATE TABLE IF NOT EXISTS subscription_plans (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.subscription_plans (
     plan_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     product_id BIGINT NOT NULL,
     tier VARCHAR(30),
@@ -42,10 +47,10 @@ CREATE TABLE IF NOT EXISTS subscription_plans (
     price DECIMAL(10,2),
     currency VARCHAR(10),
     created_at TIMESTAMP WITHOUT TIME ZONE,
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    FOREIGN KEY (product_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.products(product_id)
 );
 
-CREATE TABLE IF NOT EXISTS subscriptions (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.subscriptions (
     subscription_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
     plan_id BIGINT NOT NULL,
@@ -54,21 +59,21 @@ CREATE TABLE IF NOT EXISTS subscriptions (
     status VARCHAR(20),
     current_mrr DECIMAL(10,2),
     created_at TIMESTAMP WITHOUT TIME ZONE,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (plan_id) REFERENCES subscription_plans(plan_id)
+    FOREIGN KEY (user_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.users(user_id),
+    FOREIGN KEY (plan_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.subscription_plans(plan_id)
 );
 
-CREATE TABLE IF NOT EXISTS subscription_changes (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.subscription_changes (
     change_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     subscription_id BIGINT NOT NULL,
     old_plan_id INT,
     new_plan_id INT,
     change_type VARCHAR(20),
     change_date DATE,
-    FOREIGN KEY (subscription_id) REFERENCES subscriptions(subscription_id)
+    FOREIGN KEY (subscription_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.subscriptions(subscription_id)
 );
 
-CREATE TABLE IF NOT EXISTS payments (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.payments (
     payment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     subscription_id BIGINT NOT NULL,
     amount DECIMAL(10,2),
@@ -76,57 +81,58 @@ CREATE TABLE IF NOT EXISTS payments (
     payment_status VARCHAR(20),
     payment_date DATE,
     payment_method VARCHAR(20),
-    FOREIGN KEY (subscription_id) REFERENCES subscriptions(subscription_id)
+    FOREIGN KEY (subscription_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.subscriptions(subscription_id)
 );
 
-CREATE TABLE IF NOT EXISTS license_keys (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.license_keys (
     license_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     subscription_id BIGINT NOT NULL,
     max_seats INT,
     issued_date DATE,
     expiry_date DATE,
-    FOREIGN KEY (subscription_id) REFERENCES subscriptions(subscription_id)
+    FOREIGN KEY (subscription_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.subscriptions(subscription_id)
 );
 
-CREATE TABLE IF NOT EXISTS license_allocations (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.license_allocations (
     allocation_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     license_id BIGINT NOT NULL,
     seat_number INT,
     status VARCHAR(20),
     allocation_date DATE,
-    FOREIGN KEY (license_id) REFERENCES license_keys(license_id)
+    FOREIGN KEY (license_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.license_keys(license_id)
 );
 
-CREATE TABLE IF NOT EXISTS support_tickets (
+CREATE TABLE IF NOT EXISTS ${POSTGRES_SCHEMA_SOURCE}.support_tickets (
     ticket_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
     category VARCHAR(50),
     description TEXT,
     created_at TIMESTAMP WITHOUT TIME ZONE,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES ${POSTGRES_SCHEMA_SOURCE}.users(user_id)
 );
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${POSTGRES_USER};
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA ${POSTGRES_SCHEMA_SOURCE} TO ${POSTGRES_USER};
 
 EOSQL
 
 psql "$POSTGRES_CONN_STR" \
--c "\copy users FROM '/master_db/users.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.users FROM '/master_db/users.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy products FROM '/master_db/products.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.products FROM '/master_db/products.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy subscription_plans FROM '/master_db/plans.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.subscription_plans FROM '/master_db/plans.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy subscriptions FROM '/master_db/subscriptions.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.subscriptions FROM '/master_db/subscriptions.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy subscription_changes FROM '/master_db/changes.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.subscription_changes FROM '/master_db/changes.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy payments FROM '/master_db/payments.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.payments FROM '/master_db/payments.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy license_keys FROM '/master_db/licenses.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.license_keys FROM '/master_db/licenses.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy license_allocations FROM '/master_db/allocations.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.license_allocations FROM '/master_db/allocations.csv' CSV HEADER"
 psql "$POSTGRES_CONN_STR" \
--c "\copy support_tickets FROM '/master_db/support_tickets.csv' CSV HEADER"
+-c "\copy ${POSTGRES_SCHEMA_SOURCE}.support_tickets FROM '/master_db/support_tickets.csv' CSV HEADER"
+
 
 echo "PostgreSQL initialization completed!"
